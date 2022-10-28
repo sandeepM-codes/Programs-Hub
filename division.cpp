799bf8eaceadfb89b7b000a15acabcb2bfca9a5d
BossG21 <116905826+BossG21@users.noreply.github.com>
2022-10-28 11:20:50 -0700

    Create division.cpp

---
+++ division.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a=4,b=2,result;
+    cout<<"Enter First Number___"<<endl;
+    cin>>a;
+    
+    cout<<"Enter Second Number___"<<endl;
+    cin>>b;
+    result=a/b;
+	cout<<"Number enters are-----"<<a<<",,"<<b<<endl;
+    cout<<"Result Of Two entered Numbers Is::__ "<<result<<endl;
+    return 0;
+}
